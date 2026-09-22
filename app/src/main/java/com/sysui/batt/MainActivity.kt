6e4d525a7e86b1e33ddc18e6c1cce1eda8aa13eb
Sohan Ali <96324905+sohan-f@users.noreply.github.com>
2026-09-22 04:37:07 +0000

    refine Doto voice: round numerals, swell pulse with size

--- app/src/main/java/com/sysui/batt/MainActivity.kt
+++ app/src/main/java/com/sysui/batt/MainActivity.kt
@@ -87,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         setupOrderSelector()
         setupSizeController()
         setupActions()
+        applyDotoNumerals()
         updateSystemBarContrast()
     }
 
